5fa19f6ad5b1efcdae96740a630dc4ec4d5e69ec
Nick Fox-Gieg <nick@fox-gieg.com>
2017-08-27 14:22:17 -0400

    cleanup

--- p5js/js/sketch2.js
+++ p5js/js/sketch2.js
@@ -8,70 +8,57 @@ var sketch_2 = function(p) {
 
 	var counterX = 0;
 	var counterY = 0;
-	var stepOrig = 50;
+	var stepOrig = 100;
 	var step = stepOrig;
 	var col;
 
 	p.setup = function() {
 		p.createCanvas(512, 256);
-		d = p.pixelDensity();
 
 		img = p.createImage(p.width, p.height);
-		imgLength = 4 * (img.width * d) * (img.height * d);
+		imgLength = 4 * img.width * img.height;
 
 		col = p.randomColor();
 
-		img.loadPixels();
 		for (var i = 0; i < img.width; i++) {
 		    for (var j = 0; j < img.height; j++) {
 		        img.set(i, j, p.color(102, 90, 0, 255*((i % img.width)/img.width)));
 		    }
 		}
-		img.updatePixels();
 
+		img.updatePixels();
 		p.background(255);
 		p.image(img, 0, 0);
 	}
 
 	p.draw = function() {
-		//if (awake) {
+		if (awake) {
 			p.background(255);
-			img.loadPixels();
 
 			if (p.mouseIsPressed) {
-				img.set(p.mouseX, p.mouseY, p.color(255,0,0))
+				p.drawPlus(p.mouseX, p.mouseY);
 			}
+
 			var x = counterX;
 			var y = counterY;
 			counterX += step;
 			if (counterX > img.width) {
-				console.log("End columns.");
 				counterX = 0;
 				counterY++;
 				step++;
 			}
-			if (counterY > img.height/4) {
-				console.log("End rows.");
+			if (counterY > img.height) {
 				counterX = 0;
 				counterY = 0;
-				step = stepOrig;
+				step = p.random(stepOrig/2, stepOrig*2);
 				col = p.randomColor();
 			}
-
-			for (var i = 0; i < d; i++) {
-			  for (var j = 0; j < d; j++) {
-			    // loop over
-			    var idx = 4 * ((y * d + j) * img.width * d + (x * d + i));
-			    img.pixels[idx] = col[0];
-			    img.pixels[idx+1] = col[1];
-			    img.pixels[idx+2] = col[2];
-			    img.pixels[idx+3] = col[3];
-			  }
-			}
+			
+			p.drawPlus(x, y);
 
      		img.updatePixels();
 			p.image(img, 0, 0);
-		//}
+		}
 	}
 
 	p.mousePressed = function() {
@@ -83,7 +70,20 @@ var sketch_2 = function(p) {
 		var g = p.random(0, 255);
 		var b = p.random(0, 255);
 		var a = 255;
-		return [r, g, b, a];
+		return p.color(r, g, b, a);
+	}
+
+	p.drawPlus = function(x, y) {
+		img.set(x-2, y, col);
+		img.set(x-1, y, col);
+		img.set(x, y, col);
+		img.set(x+1, y, col);
+		img.set(x+2, y, col);
+		img.set(x, y-2, col);
+		img.set(x, y-1, col);
+		img.set(x, y, col);
+		img.set(x, y+1, col);
+		img.set(x, y+2, col);
 	}
 
 }
